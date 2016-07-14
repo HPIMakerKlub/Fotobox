a7c99ddc21b6614f3461bebf6ef08c0596976cbe
Fluepke <fabian.luepke@gmail.com>
2016-07-15 00:36:38 +0200

    Point maps to HPI

--- js/grayscale.js
+++ js/grayscale.js
@@ -39,7 +39,7 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(52.2361, 13.7933));
 });
 
 function init() {
@@ -50,7 +50,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(52.2361, 13.7933), // HPI
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
